8523e2e57f7746a23385c6eb122ebad6aa61ef6f
Pascal Papot <sourcetree@papot.be>
2020-11-14 00:49:33 +0100

    registered and blind

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -14,10 +14,11 @@ export class ExamplePlatformAccessory {
    * These are just used to create a working example
    * You should implement your own code to track the state of your accessory
    */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+  private state = {
+    lastPosition : 100,
   };
+
+ 
 
   constructor(
     private readonly platform: ExampleHomebridgePlatform,
@@ -31,50 +32,31 @@ export class ExamplePlatformAccessory {
       .setCharacteristic(this.platform.Characteristic.SerialNumber, accessory.context.device.voletUniqueId)
       .setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.voletName);
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
+    // get the WindowCovering service if it exists, otherwise create a new WindowCovering service
     // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.service = this.accessory.getService(this.platform.Service.WindowCovering) ||
+                   this.accessory.addService(this.platform.Service.WindowCovering);
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.voletName);
 
     // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+    // see https://developers.homebridge.io/#/service/WindowCovering
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    // register handlers for the Target Position Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.TargetPosition)
+      .on('set', this.handleTargetPositionSet.bind(this))                // SET - bind to the `setOn` method below
+      .on('get', this.handleTargetPositionGet.bind(this));               // GET - bind to the `getOn` method below
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    // register handlers for the CurrentPosition Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentPosition)
+      .on('get', this.handleCurrentPositionGet.bind(this));       // GET - bind to the 'setBrightness` method below
 
-
-    /**
-     * Creating multiple services of the same type.
-     * 
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     * 
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-
-
-
-    /**
-     * Updating characteristics values asynchronously.
-     * 
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     * 
-     */
+    // register handlers for the PositionState Characteristic
+    this.service.getCharacteristic(
+      this.platform.Characteristic.PositionState).updateValue(this.platform.Characteristic.PositionState.STOPPED);
+ 
     
   }
 
@@ -82,57 +64,27 @@ export class ExamplePlatformAccessory {
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
    */
-  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+  handleTargetPositionSet(value: CharacteristicValue, callback: CharacteristicSetCallback) {
 
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
-    
+    if ((value > this.state.lastPosition) || (this.state.lastPosition === 0)){
+      this.platform.log.debug('moveup');
+      this.state.lastPosition = 100;
+    } else {
+      this.platform.log.debug('movedown');
+      this.state.lastPosition = 0;
+    }
 
-    this.platform.log.debug('Set Characteristic On ->', value);
-
-    // you must call the callback function
     callback(null);
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   * 
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   * 
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
 
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  getOn(callback: CharacteristicGetCallback) {
-
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // you must call the callback function
-    // the first argument should be null if there were no errors
-    // the second argument should be the value to return
-    callback(null, isOn);
+  handleTargetPositionGet(callback: CharacteristicGetCallback) {
+    callback(null, this.state.lastPosition);
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
-
-    // you must call the callback function
-    callback(null);
+ 
+  handleCurrentPositionGet(callback: CharacteristicSetCallback) {
+    callback(null, this.state.lastPosition);
   }
 
 }
