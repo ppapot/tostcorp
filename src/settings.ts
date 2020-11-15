0159759ce89d73e1cd1622040f1839a0433d13de
Pascal Papot <sourcetree@papot.be>
2020-11-15 14:26:37 +0100

    change .md

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'TostCorp';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-TostCorp-RTS';
+export const PLUGIN_NAME = 'homebridge-TostCorp';
